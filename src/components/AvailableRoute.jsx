3f34f15d8171ee81bd678221339cd1de03dce842
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2024-09-10 18:17:38 +0300

    Update metadata when switch current service

--- src/components/AvailableRoute.jsx
+++ src/components/AvailableRoute.jsx
@@ -9,16 +9,40 @@ import { isServiceAvailable } from "../utils/availability";
 import { loadServiceTranslationsByServiceName } from "../utils/loadServiceTranslationsByServiceName";
 import ErrorScreen from "./Error";
 
+const changeMetaData = (serviceInfo) => {
+  if (!serviceInfo) {
+    document.title = "Home";
+  } else {
+    document.title = serviceInfo.display_name;
+    const description = [...document.getElementsByTagName("META")].find(
+      (tag) => tag.name === "description",
+    );
+    if (description) {
+      description.setAttribute("content", serviceInfo.description);
+    } else {
+      const metaDescription = document.createElement("meta");
+      metaDescription.setAttribute("name", "description");
+      metaDescription.setAttribute("content", serviceInfo.description);
+    }
+  }
+};
+
 const AvailableRoute = () => {
   const currentServiceName = useSelector((state) => state.host.currentService);
   const location = useLocation();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.host.user);
   const currentService = useSelector((state) => state.host.currentService);
+  const fullAccountsInfo = useSelector((state) => state.host.fullAccountsInfo);
   const currentRoute = location.pathname.split("/")[1];
+  const currentServiceInfo =
+    fullAccountsInfo[user.account].servicesInLocations[user.location][
+      currentService
+    ];
 
   useEffect(() => {
     loadServiceTranslationsByServiceName(currentServiceName || HOME);
+    changeMetaData(currentServiceInfo);
   }, []);
 
   useEffect(() => {
@@ -27,6 +51,10 @@ const AvailableRoute = () => {
       loadServiceTranslationsByServiceName(currentRoute || HOME);
     }
   }, [currentRoute]);
+
+  useEffect(() => {
+    changeMetaData(currentServiceInfo);
+  }, [currentService]);
 
   useEffect(() => {
     dispatch(fetchLocationData(user.location));
