bb8b2243063a72822f74488031c769143e10626c
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2025-06-27 16:51:40 +0300

    Add log

--- src/shared/ui/combobox.tsx
+++ src/shared/ui/combobox.tsx
@@ -76,6 +76,7 @@ export const Combobox = React.forwardRef<HTMLButtonElement, Combobox>(
 			text: item.text + "",
 		}));
 		const formattedValue = value ? value + "" : value;
+		console.log("ku");
 
 		React.useEffect(() => {
 			if (!searchQueryFn) return;
