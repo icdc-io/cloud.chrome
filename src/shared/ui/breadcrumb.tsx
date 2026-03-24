6ddbeb3a630f736d1d0cfb8ff47e10fe6c24afd0
Dzmitry Valashchuk <DValashchuk@ibagroup.eu>
2026-03-24 11:17:36 +0300

    Update biome, format files with new rules

--- src/shared/ui/breadcrumb.tsx
+++ src/shared/ui/breadcrumb.tsx
@@ -108,10 +108,10 @@ BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
 
 export {
 	Breadcrumb,
-	BreadcrumbList,
+	BreadcrumbEllipsis,
 	BreadcrumbItem,
 	BreadcrumbLink,
+	BreadcrumbList,
 	BreadcrumbPage,
 	BreadcrumbSeparator,
-	BreadcrumbEllipsis,
 };
